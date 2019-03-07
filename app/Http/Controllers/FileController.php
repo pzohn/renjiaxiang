e89dbe864a713ec83dc7a8e479be3de9283350e8
pzohn <pzohn@163.com>
2019-03-07 10:58:47 +0800

    20190307-1

--- app/Http/Controllers/FileController.php
+++ app/Http/Controllers/FileController.php
@@ -20,7 +20,7 @@ class FileController extends Controller
             $v->img
             ];
         }
-        $img_url = $img_urls[array_rand($img_urls,1)]; //"https://www.hattonstar.com/card/".
+        $img_url = array_rand($img_urls,1); //"https://www.hattonstar.com/card/".$img_urls[array_rand($img_urls,1)];
         return $img_url;
          $file = $req->file('file');
          if($file->isValid()) {
