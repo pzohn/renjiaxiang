e9067ff78b2f9063fcae6aa3a9bbbd962cf9eada
pzohn <pzohn@163.com>
2019-10-23 10:41:37 +0800

    20191023-hubin-cs2

--- app/Http/Controllers/PayController.php
+++ app/Http/Controllers/PayController.php
@@ -635,9 +635,7 @@ class PayController extends Controller
 
                 if ($count){
                     $tradesTmp[] = [
-                        "time" => strtotime($v->updated_at->format('Y-m-d H:i:s')),
-                        "time1" => strtotime($v->updated_at),
-                        "time2" => $v->updated_at->format('Y-m-d H:i:s'),
+                        "time" => $v->updated_at->format('Y-m-d H:i:s')
                         "tradeid" => $v->out_trade_no,
                         "charge" => $v->total_fee,
                         "count" => $count,
