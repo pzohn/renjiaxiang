225f25a8643aa25433f7806c9ecd8ae0f46c59f7
pzohn <pzohn@163.com>
2020-02-17 12:51:25 +0800

    20200217-1

--- app/Http/Controllers/ImageController.php
+++ app/Http/Controllers/ImageController.php
@@ -26,7 +26,7 @@ class ImageController extends Controller
                 ];
             }
             $access_token = $resultToken['access_token'];
-            return $resultToken;
+            return $access_token;
             if ($access_token) {
                 $urlOcr = "https://aip.baidubce.com/rest/2.0/ocr/v1/general";
                 $params = [
