5dabbc1378bf74fd9108ff935ecd6cc0b0719ae5
pzohn <pzohn@163.com>
2020-04-02 23:00:31 +0800

    20200402-1

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,7 @@ Route::get('/', function () {
 });
 
 Route::get('/hh', function () {
-    return view('welcome');
+    return view('hubin');
 });
  
 Route::post('/user/save', 'UserController@setUser');
