43c98233dc868a9ad0ff43869aa96c64a44067ef
pzohn <pzohn@163.com>
2020-07-08 16:22:59 +0800

    20200708-5

--- app/Models/Tradetmp.php
+++ app/Models/Tradetmp.php
@@ -17,10 +17,10 @@ class  Tradetmp extends Model {
         $tradetmp->shop_id = array_get($params,"shop_id");
         $tradetmp->name = array_get($params,"name");
         if (($tradetmp->detail_id >= 51) && ($tradetmp->detail_id <= 59)){
-            $info = Information::getInformationEx($tradetmp->phone);
-            if ($info){
-                $tradetmp->name = $info->SCHOOL . $info->CLASS . $tradetmp->name;
-            }
+            // $info = Information::getInformationEx($tradetmp->phone);
+            // if ($info){
+            //     $tradetmp->name = $info->SCHOOL . $info->CLASS . $tradetmp->name;
+            // }
         }
         $tradetmp->save();
         return $tradetmp;
