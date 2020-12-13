8b161af86f0bb858fa7e2e35493ae6a42af1d92f
pzohn <pzohn@163.com>
2020-12-13 18:38:33 +0800

    20201213-3

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -432,7 +432,7 @@ class UserController extends Controller
                     $second = true;
                     $parter1 =  Parter::getParterForId($v->id);
                     $parter2 =  Parter::getParterForId($parter1->share_parent_id);
-                    $partersSecond = [
+                    $partersSecond[] = [
                         "name" => $v->name,
                         "id" => $v->id
                     ];
