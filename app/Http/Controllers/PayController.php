f32cee445d1f0fbbf9a4968c65fd6cce3d224504
pzohn <pzohn@163.com>
2021-01-03 15:03:52 +0800

    20210103-3

--- app/Http/Controllers/PayController.php
+++ app/Http/Controllers/PayController.php
@@ -429,12 +429,27 @@ class PayController extends Controller
                 $childtrades = Childtrade::paySelectById($v->id);
                 $childtradesTmp = [];
                 foreach ($childtrades as $k1 => $v1) {
+                    // $shopping = Shopping::shoppingSelect($v1->shopping_id);
+                    // if ($shopping){
+                    //     $count += 1;
+                    //     $childtradesTmp[] = [
+                    //         "name" => $shopping->name,
+                    //         "charge" => $shopping->price ,
+                    //         "title_pic" => Image::GetImageUrlByParentId($shopping->id,$title,$shopping->type),
+                    //         "shopping_id" => $shopping->id,
+                    //         "num" => $v1->num
+                    //     ]; 
+                    // }
                     $shopping = Shopping::shoppingSelect($v1->shopping_id);
                     if ($shopping){
                         $count += 1;
+                        $retail_price = $v1->retail_price;
+                        if ($retail_price == 0){
+                            $retail_price = $shopping->price;
+                        }
                         $childtradesTmp[] = [
                             "name" => $shopping->name,
-                            "charge" => $shopping->price ,
+                            "charge" => $retail_price,
                             "title_pic" => Image::GetImageUrlByParentId($shopping->id,$title,$shopping->type),
                             "shopping_id" => $shopping->id,
                             "num" => $v1->num
