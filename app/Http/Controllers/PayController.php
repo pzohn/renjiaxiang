2bb7a2be9881e179041e3b79fbf42b37e5827a05
pzohn <pzohn@163.com>
2021-01-03 13:50:40 +0800

    20210103-3

--- app/Http/Controllers/PayController.php
+++ app/Http/Controllers/PayController.php
@@ -669,13 +669,9 @@ class PayController extends Controller
                     $shopping = Shopping::shoppingSelect($v1->shopping_id);
                     if ($shopping){
                         $count += 1;
-                        $retail_price = $v1->retail_price;
-                        if ($retail_price == 0){
-                            $retail_price = $shopping->price;
-                        }
                         $childtradesTmp[] = [
                             "name" => $shopping->name,
-                            "charge" => $retail_price ,
+                            "charge" => $shopping->price ,
                             "title_pic" => Image::GetImageUrlByParentId($shopping->id,$title,$shopping->type),
                             "shopping_id" => $shopping->id,
                             "num" => $v1->num
