b906ec4c4cf33b9fab77ed01bf9d30b3e34dd05a
pzohn <pzohn@163.com>
2021-01-27 21:01:57 +0800

    20210127-1

--- app/Http/Controllers/ExpressController.php
+++ app/Http/Controllers/ExpressController.php
@@ -144,7 +144,7 @@ class ExpressController extends Controller
             'service' => $service
         ];
         $urlAdd = 'https://api.weixin.qq.com/cgi-bin/express/business/order/add?access_token='.$access_token;
-        $resultAdd = GuzzleHttp::guzzlePost($urlToken, $paramsAdd);
+        $resultAdd = GuzzleHttp::guzzlePost($resultAdd, $paramsAdd);
         if (isset($resultAdd['errcode']))
         {
             return [
