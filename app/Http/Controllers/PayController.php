73b407ea45dd73ced1f93941545badd8fc617502
pzohn <pzohn@163.com>
2021-02-27 10:07:03 +0800

    20210227-1

--- app/Http/Controllers/PayController.php
+++ app/Http/Controllers/PayController.php
@@ -1857,6 +1857,7 @@ class PayController extends Controller
     }
 
     public function getShareForZhaoboEx(Request $req) {
+        return $req->get('shop_id');
         $zhang = Zhang::getZhang($req->get('shop_id'));
         // if($zhang == 0){
         //     return [
